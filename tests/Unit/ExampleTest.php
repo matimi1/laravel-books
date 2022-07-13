b193a0244b5925293cbfb7a0663f8d195bfb5d31
Jan Polak <jan.polak@onmyway.cz>
2022-07-13 10:09:08 +0200

    fixed unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,18 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// the test in this file will be run without setting up
+// a laravel application - faster, simpler, but with no
+// database access etc. Useful for small units that
+// work independently of the application:
+// use PHPUnit\Framework\TestCase;
+
+// the tests in this file will first initialize a
+// Laravel application so that all the features of the
+// project can be tested. Useful for units that use
+// the application's functions:
+use Tests\TestCase;
+
 use App\Models\Book;
 
 class ExampleTest extends TestCase
@@ -17,10 +28,10 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 
-    // public function test_book_can_be_found_in_database()
-    // {
-    //     $book = Book::find(1);
+    public function test_book_can_be_found_in_database()
+    {
+        $book = Book::find(1);
 
-    //     $this->assertTrue($book->id === 1);
-    // }
+        $this->assertTrue($book->id === 1);
+    }
 }
